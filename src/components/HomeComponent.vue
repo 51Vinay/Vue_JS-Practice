24a548b0950fe0b68231c1c017f2f6023b947f5f
vinay sharma <vinaysharma5335@gmail.com>
2023-11-09 15:31:36 +0530

    Third Commit

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
         <h1>Home Componenet</h1>
+        <h2>Email: {{ email }}</h2>
+        <h2>Phone:{{ phone }}</h2>
     </div>
 </template>
 <script>
 export default{
-    name:'HomeComponent'
+    name:'HomeComponent',
+    data(){
+        return{
+            email:"abc@gmail.com",
+            phone:"7867564534"
+        }
+    }
 }
 </script>
