9c93ab99385e8acdd6ba8209651e84effd7281bf
vinay sharma <vinaysharma5335@gmail.com>
2023-11-09 16:22:07 +0530

    Fourth Commit

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -3,6 +3,8 @@
         <h1>Home Componenet</h1>
         <h2>Email: {{ email }}</h2>
         <h2>Phone:{{ phone }}</h2>
+        <h2>Name:{{ getName("Vinay") }}</h2>
+        <h2>Old Data :{{ getData() }}</h2>
     </div>
 </template>
 <script>
@@ -10,9 +12,22 @@ export default{
     name:'HomeComponent',
     data(){
         return{
-            email:"abc@gmail.com",
+            email:"abcd@gmail.com",
             phone:"7867564534"
         }
+    },
+    methods:{
+        getName(name)
+        {
+            return name;
+        },
+        getData(){
+            return {
+                Name:"abc",
+                email: this.email
+            }
+        }
+
     }
 }
 </script>
